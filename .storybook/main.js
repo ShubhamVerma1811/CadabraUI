0c5a2031bbb7918c12b5f618eeddd78c383ee63f
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-05-17 15:54:31 +0530

    re-added storybook dark mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
+    'storybook-dark-mode'
   ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
